aceaa0ae03f21878f2dba8a187a5fe3b6115c539
Maria Sosa <mariasosal14@gmail.com>
2019-04-19 15:01:55 -0500

    Geodesia Geometrica, calculo de coordenadas y,z

--- src/geodesia/geometrica/CalcularCoordenadas.java
+++ src/geodesia/geometrica/CalcularCoordenadas.java
@@ -17,37 +17,36 @@ public class CalcularCoordenadas {
     double grados=0;
     double minutos=0;
     double segundos=0;
-    double latitud;
-    double a=6378388; //semieje a
-    double e=0.00672267;//Exceso esferico al cuadrado
-    double coseno;
-    double seno;
-    
-    void ConvertirGrados (double unaLatitud){
+    double latitud ;
+   
+        double ConvertirGrados (){ 
         latitud= (grados)+(minutos/60)+(segundos/3600); 
+        return latitud; 
+    }
         
-    }
-    
-    void ConvertirAngulo(double unangulo){
-        latitud=Math.toRadians(unangulo);
-    }
-    double seno(){
-        return sin(latitud);
-    }
-    
-    double coseno(){
-        return cos(latitud);
-    }
-    void CalcularY(){
-    double y;
-    y=((a*coseno)/(Math.pow(1-e*Math.pow(seno,2),1/2)));
-    }
-    void CalcularZ(){
-    double z;
-    z=(((a*1-e)*seno)/(Math.pow(1-e*Math.pow(seno,2),1/2)));
-    }
+       void ConvertirAngulo(double unangulo){
+         latitud =Math.toRadians(unangulo);
+           
+       }
+//      double seno(){
+//           return sin(latitud);
+//       }
+//      double coseno(){
+//           return cos(latitud);
+//       }
+       
+    double a=6378388; //semieje a
+    double e=0.00672267;//primera excentricidad al cuadrado
 
-    Object ConvertirAngulo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    double CalcularY(){
+    double y;
+    y=a*(1-e)*cos(latitud)/(Math.pow(1-e*Math.pow(sin(latitud),2),1/2));
+    return y;
+    }
+    double CalcularZ(){
+    double z;
+    z=a*(1-e)*sin(latitud)/(Math.pow(1-e*Math.pow(sin(latitud),2),1/2));
+    return z;
     }
 }
